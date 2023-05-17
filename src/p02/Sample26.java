5572cb885f59e6d9ce5089a4c319cc88657d1931
student <student@WIN-U15ILJ4R9O8>
2023-05-17 11:46:17 +0900

    Sample27追加

--- src/p02/Sample26.java
+++ src/p02/Sample26.java
@@ -3,7 +3,9 @@ package p02;
 public class Sample26 {
 
 	public static void main(String[] args) {
-		System.out.println("AAA");
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
---
+++ src/p02/Sample27.java
@@ -0,0 +1,13 @@
+package p02;
+
+public class Sample27 {
+
+	public static void main(String[] args) {
+		String[] id = {"岡田", "谷口", "野澤"};
+		
+		for (int i = 0; i < id.length; i++) {
+			System.out.println(id[i]);
+		}
+	}
+
+}
