706afe79c36065008348d62e31329598edbb20ae
student <student@WIN-U15ILJ4R9O8>
2023-05-17 14:09:09 +0900

    Mondai08_01追加

---
+++ src/mon/Mondai08_01.java
@@ -0,0 +1,15 @@
+package mon;
+
+public class Mondai08_01 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1から10までの合計は" + sum + "です。");
+	}
+
+}
