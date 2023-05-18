7fea635c115ec4e7b83bee4d85492140a8e790d7
student <student@WIN-U15ILJ4R9O8>
2023-05-18 10:14:15 +0900

    Sample35と36追加

---
+++ src/p02/Sample35.java
@@ -0,0 +1,19 @@
+package p02;
+
+public class Sample35 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println();
+		
+		for (int i = 5; i >= 1; i--) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
---
+++ src/p02/Sample36.java
@@ -0,0 +1,20 @@
+package p02;
+
+public class Sample36 {
+
+	public static void main(String[] args) {
+		int[] list = {10, 20, 30};
+
+		for (int i = 0; i < list.length; i++) {
+			System.out.println(list[i]);
+		}
+		
+		System.out.println();
+		
+		for (int i = list.length -1; i >= 0; i--) {
+			System.out.println(list[i]);
+		}
+		
+	}
+
+}
