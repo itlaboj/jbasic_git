6ca8f6aac8f52ab212eda7f2475a704a0a0b2fc9
student <student@WIN-U15ILJ4R9O8>
2023-05-24 13:35:57 +0900

    DAOプログラム

---
+++ src/db01/MemberDAO.java
@@ -0,0 +1,60 @@
+package db01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MemberDAO {
+	//DB接続情報の宣言
+	private String url = "jdbc:postgresql:id01"; //DB
+	private String user = "postgres";
+	private String pass = "himitu";
+
+	//単一検索用メソッド
+	public Member find(int _id) {
+		try {
+			//JDBCドライバ登録
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBCドライバが利用できません！");
+			e.printStackTrace();
+		} 
+		
+		//実行するSQL文
+		String sql = "SELECT id, name, age FROM Members WHERE id = ?";
+		
+		Member m = null;
+		
+		try (
+			//データベースへの接続
+			Connection con = DriverManager.getConnection(url, user, pass);
+				
+			//SQL文の実行の準備
+			PreparedStatement ps = con.prepareStatement(sql);
+		) {
+			//プレースホルダに値を設定
+			ps.setInt(1, _id);
+			
+			//SQL文の実行と結果の取得
+			ResultSet rs = ps.executeQuery();
+			
+			
+			//カーソルを移動しながらレコードを表示
+			if (rs.next() == true) {
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				
+				m =  new Member(id, name, age);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return m;
+	}
+
+}
